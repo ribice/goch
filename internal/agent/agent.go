8211e91e775fed4402ecaa27b7779798b2faa9b8
Emir Ribic <ribice@gmail.com>
2019-07-11 15:02:03 +0200

    Fix error shadowing

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -102,6 +102,7 @@ func (a *Agent) HandleConn(conn *websocket.Conn, req *initConReq) {
 	mc := make(chan *goch.Message)
 	{
 		var close func()
+		var err error
 
 		if req.LastSeq != nil {
 			close, err = a.mb.Subscribe(req.Channel, user.UID, *req.LastSeq, mc)
